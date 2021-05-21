14614972a44083630ada6d546cfd6c195833bfb4
rastafaninplakeibol <rastafaninplakeibol@gmail.com>
2021-05-21 12:24:04 +0200

    Update torch.ts

--- torch.ts
+++ torch.ts
@@ -1,4 +1,9 @@
-function Torch(props: { enabled: boolean }) {
+import * as React from 'react'
+import { useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
+import { Camera } from 'expo-camera';
+
+export default function Torch(props: { enabled: boolean }) {
 	const [enabled, setEnabled] = useState(props.enabled)
 	const [hasPermission, setHasPermission] = useState(false);
 	const type = Camera.Constants.Type.back;
@@ -24,5 +29,3 @@ function Torch(props: { enabled: boolean }) {
 		!enabled ? <View /> : <Camera style={{ width: 1, height: 1 }} type={type} flashMode={'torch'} />
 	);
 }
-
-module.exports = Torch
