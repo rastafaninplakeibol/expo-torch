cab99642fa103db942f9f930f567005abcaaf696
rastafaninplakeibol <rastafaninplakeibol@gmail.com>
2021-05-21 12:13:04 +0200

    Create torch.ts

---
+++ torch.ts
@@ -0,0 +1,26 @@
+function Torch(props: { enabled: boolean }) {
+	const [enabled, setEnabled] = useState(props.enabled)
+	const [hasPermission, setHasPermission] = useState(false);
+	const type = Camera.Constants.Type.back;
+
+	useEffect(() => {
+		(async () => {
+			const { status } = await Camera.requestPermissionsAsync();
+			setHasPermission(status === 'granted');
+		})();
+	}, []);
+
+	useEffect(() => {
+		setEnabled(props.enabled)
+	}, [props])
+
+	if (hasPermission === null) {
+		return <View />;
+	}
+	if (hasPermission === false) {
+		return <Text>No access to camera</Text>;
+	}
+	return (
+		!enabled ? <View /> : <Camera style={{ width: 1, height: 1 }} type={type} flashMode={'torch'} />
+	);
+}
